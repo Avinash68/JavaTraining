44d34a6d10319fe8c39bfffb9d62d7a2698b89dc
avinasht <avinash.thorat@vertexinc.com>
2023-04-19 13:04:25 +0530

    code cleanup

---
+++ CalculatorApp/src/Maths.java
@@ -0,0 +1,23 @@
+
+public class Maths {
+	public void add(int a, int b) {
+		int result = a + b;
+		System.out.println("The addition is " + result);
+	}
+
+	public void multiply(int a, int b) {
+		int result = a * b;
+		System.out.println("The multiplication is " + result);
+	}
+
+	public void square(int a) {
+		int result = a * a;
+		System.out.println("The square is " + result);
+	}
+
+	public void division(int a, int b) {
+		float result = a / b;
+		System.out.println("The division is " + result);
+	}
+
+}
